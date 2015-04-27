4c52dec45235c432ce2262bdc0c236741b9557f3
Sean McArdle <sean.mcardle0@gmail.com>
2015-04-26 20:57:35 -0700

    Inital Flask setup.

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='jbrecipes',
       version='1.0',
-      description='OpenShift App',
-      author='Your Name',
+      description='JupiterBroadcasting Recipes',
+      author='Sean McArdle',
       author_email='example@example.com',
       url='https://www.python.org/community/sigs/current/distutils-sig',
-      install_requires=['Flask>=0.7.2', 'MarkupSafe'],
+      install_requires=['Flask','flask-wtf','flask-babel','markdown','flup'],
      )
